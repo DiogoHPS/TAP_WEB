ed107b6ba66af681405ef182193524c478d2527f
DiogoHPS <diogoambura@gmail.com>
2024-05-06 15:30:13 -0300

    cadastro de usuarios utilizando cookies

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 function register() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    // Aqui você pode adicionar a lógica de validação e envio dos dados
-  
-    // Exemplo de saída
-    console.log("Usuário: " + username + ", Senha: " + password);
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var matricula = document.getElementById("matricula").value;
+  var password = document.getElementById("password").value;
+  var rpassword = document.getElementById("rpassword").value;
+
+  // Verifica se as senhas coincidem
+  if (password !== rpassword) {
+      alert("As senhas não coincidem.");
+      return;
   }
 
-  function irParaLogin() {
-    window.location.href = 'login.html';
-  }
+  // Armazena os dados do usuário em cookies
+  document.cookie = "username=" + encodeURIComponent(username);
+  document.cookie = "email=" + encodeURIComponent(email);
+  document.cookie = "matricula=" + encodeURIComponent(matricula);
+  document.cookie = "password=" + encodeURIComponent(password);
+
+  // Redireciona para a página de login
+  window.location.href = 'login.html';
+}
+
 
   function irParaCadastro() {
     window.location.href = 'cadastro.html';
