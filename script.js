11f95a40fdc1d076b296f43baf9ef5913795ffb4
DiogoHPS <diogoambura@gmail.com>
2024-05-07 14:20:00 -0300

    feito as mudanças no front e criado o index

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 function register() {
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("email").value;
-  var matricula = document.getElementById("matricula").value;
-  var password = document.getElementById("password").value;
-  var rpassword = document.getElementById("rpassword").value;
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var matricula = document.getElementById("matricula").value;
+    var password = document.getElementById("password").value;
+    var rpassword = document.getElementById("rpassword").value;
 
-  // Verifica se as senhas coincidem
-  if (password !== rpassword) {
-      alert("As senhas não coincidem.");
-      return;
-  }
+    // Verifica se as senhas coincidem
+    if (password !== rpassword) {
+        alert("As senhas não coincidem.");
+        return;
+    }
 
-  // Armazena os dados do usuário em cookies
-  document.cookie = "username=" + encodeURIComponent(username);
-  document.cookie = "email=" + encodeURIComponent(email);
-  document.cookie = "matricula=" + encodeURIComponent(matricula);
-  document.cookie = "password=" + encodeURIComponent(password);
+    // Armazena os dados do usuário em cookies
+    document.cookie = "username=" + encodeURIComponent(username);
+    document.cookie = "email=" + encodeURIComponent(email);
+    document.cookie = "matricula=" + encodeURIComponent(matricula);
+    document.cookie = "password=" + encodeURIComponent(password);
 
-  // Redireciona para a página de login
-  window.location.href = 'login.html';
+    // Redireciona para a página de login
+    window.location.href = 'login.html';
 }
 
+function irParaLogin() {
+    window.location.href = 'login.html';
+}
 
-  function irParaCadastro() {
-    window.location.href = 'cadastro.html';
-  }
+function irParaindex() {
+    window.location.href = 'index.html';
+}
